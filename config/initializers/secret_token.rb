3986ea206368489b279e831e216e768cf365b9e0
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-12 04:55:09 -0600

    [add] setup project :heart_eyes:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiblogApp::Application.config.secret_key_base = '3152f6760f7789c520bd50ac0a65ca044146c45ee711f94404bb7406a8b6c98b68384f02a7b747e64ae46cf467eeeb02e4af6eab8e777654e78f22b2c8696782'
